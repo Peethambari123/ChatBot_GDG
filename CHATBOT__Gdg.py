dbe0f02c89d53782868a7eb0d1cf440dd6ae2eda
Peethambari123 <preethimanavarti550@gmail.com>
2025-03-11 18:19:27 +0530

    Update CHATBOT__Gdg.py

--- CHATBOT__Gdg.py
+++ CHATBOT__Gdg.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-API_KEY= AIzaSyDpu9A3HRczGBYE0h44SpqSwEfq-5bDDEQ
+API_KEY= AIzaSyCl2590lMvoFHLhjfGWyTJKxDbONDpFauw
 
 genai.configure(api_key=API_KEY)
 model=genai.GenerativeModel('gemini-1.5-flash')
